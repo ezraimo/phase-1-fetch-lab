4adaff2f54fcc04eaf98c00df973b5b636304a5f
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-31 14:32:07 +0300

    fetch apis

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Fetch data from the Game of Thrones API and return the fetch() promise
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json()) // Convert response to JSON
+    .then(books => renderBooks(books)) // Pass JSON data to renderBooks
+    .catch(error => console.error("Error fetching books:", error));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
   books.forEach(book => {
-    const h2 = document.createElement('h2');
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
